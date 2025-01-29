59c76a4d785b0a0807ebc59cbe11ec3e48c4a4de
MohamedUmar083 <umarmohd777@gmail.com>
2025-01-29 20:25:16 +0400

    Checking new Pull Request

--- src/Pages/All.jsx
+++ src/Pages/All.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Cards from "../Components/Cards/Cards";
 
 const All = ({ data }) => {
+  // New Command to check git pull request
   return (
     <div className="container">
       <h3>All Program in One Place</h3>
